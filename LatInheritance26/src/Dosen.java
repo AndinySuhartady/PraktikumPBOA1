81669b4b5dc69e0b6705f8b62b128783183bfb3e
AndinySuhartady <suhartadyandiny@gmail.com>
2026-05-01 22:14:35 +0700

    Hapus folder tidak diperlukan dari repo

--- LatInheritance26/src/Dosen.java
+++
@@ -1,8 +0,0 @@
-class Dosen extends Pegawai {
-    protected String fakultas;
-
-    public Dosen(String nip, String nama, String tglLahir, String tmtBekerja, double gajiPokok, String fakultas) {
-        super(nip, nama, tglLahir, tmtBekerja, gajiPokok);
-        this.fakultas = fakultas;
-    }
-}
